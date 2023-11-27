7468976db34af72327f3274b8bbac33f91312775
Kazi Tajnur Islam <tajnur007@gmail.com>
2023-11-27 23:53:24 +0600

    Redux 1st class tutorial addded

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,47 @@ import ThemeProvider from './ThemeProvider.jsx';
 import { router } from './routes.jsx';
 
 import './global.css';
+import { Provider } from 'react-redux';
+import { createStore } from '@reduxjs/toolkit';
+
+
+const reducer = (state, action) => {
+  if (action.type === 'increment') {
+    state = state + 1;
+  } else if (action.type === 'decrement') {
+    state = state - 1;
+  } else if (action.type === 'reset') {
+    state = 0;
+  } else {
+    throw new Error('Unknown action');
+  }
+
+  // switch(action.type) {
+  //   case 'increment':
+  //     state = state + 1;
+  //     break;
+  //   case 'decrement':
+  //     state = state - 1;
+  //     break;
+  //   case 'reset':
+  //     state = 0;
+  //     break;
+  //   default:
+  //     throw new Error('Unknown action');
+  // }
+
+  return state;
+};
+
+const store = createStore(reducer, 0, undefined);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
 )
